6e1084c96143e07d953b7b24bebd07bb8d679772
Ashley Barr <ashley.barr@ar4s.com>
2020-12-16 21:05:05 +0000

    added .red class to character counter when user exceeds 140 characters, moved script to composer-char-counter.js in scripts

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,21 @@
 console.log(`composer-char-counter loaded`)
 
-$(() => {
-  const $tweetText = $("#tweet-text"); 
-  const update = () => {
-    const chars = 140 - $tweetText.val().length;
-    const $counter = $("#counter")
-    $counter.text(chars)
-  };
-  $tweetText.on("keyup", update);
+$(document).ready(function() {
+  console.log(`Ready Document One`)
+  // Select the id to assign the event listener 
+  // Use "input" so it update on every char change
+  $("#tweet-text").on("input", function () {
+    // Length is the value of #text-tweet chars
+    let length = $(this).val().length;
+    // Grab the value of counter 
+    let counter = $(".counter");
+    counter.val(140 - length);
+    // Change the counter to red if negative
+    console.log(length)
+    if (length > 140) {
+      counter.addClass("red");
+    } else if (length < 141) {
+      counter.removeClass("red");
+    }
+  })
 });
