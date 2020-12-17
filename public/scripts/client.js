e68bf365ec21182ebc72c1337a00fde737c00301
ar4sdoteth <ashley.barr@ar4s.com>
2020-12-17 14:03:05 -0700

    implemented the loadTweets function in client.js. now pulling tweets from the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 console.log(`client.js loaded`)
 
 // Temporary faux database
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $(document).ready(function() {
   console.log(`Ready Client Document One`)
@@ -37,6 +37,7 @@ $(document).ready(function() {
   // to createTweetElement
   const renderTweets = (tweetArray) => {
     for (let eachTweet of tweetArray) {
+      console.log(`each tweet`, eachTweet)
       $tweetObj = createTweetElement(eachTweet);
     }
   }
@@ -57,15 +58,24 @@ $(document).ready(function() {
     </article>`);
     $('#tweets-container').append($tweet);
   }
-  
+
   // Use ajax to send form && prevent
   // page reload
   $("form").submit(function(event) {
     event.preventDefault();
     $.ajax({ type: "POST", url: "/tweets", data: $(this).serialize() })
-    .then(console.log("It works!"));
+    .then(console.log("AJAX workz"));
   });
 
-  const $tweet = renderTweets(data);
+  // Use ajax to load the database
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET'})
+    .then((fetchTweets) => {
+      console.log(`Got the tweets`, fetchTweets);
+      renderTweets(fetchTweets)
+    });
+  }
+
+  loadTweets();
 
 });
