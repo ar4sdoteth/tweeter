17981675f1b5a6fda5db56beeb16c27f6054a6d1
ar4sdoteth <ashley.barr@ar4s.com>
2020-12-17 14:03:58 -0700

    deleted the temp/faux database from client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
 */
 console.log(`client.js loaded`)
 
-// Temporary faux database
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 $(document).ready(function() {
   console.log(`Ready Client Document One`)
   // Loops through an array of tweets 
