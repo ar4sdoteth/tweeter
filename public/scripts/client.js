0d6a746bc55d1f329918ae5b6c23e88f5e3eebd2
ar4sdoteth <ashley.barr@ar4s.com>
2020-12-19 11:36:33 -0700

    code clean up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,14 +3,15 @@
 * jQuery is already loaded
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
-console.log(`client.js loaded`)
 
 $(document).ready(function() {
-  console.log(`Ready Client Document One`)
+
 
   // Event handler to initiate tweet
   $('#start-tweet').click(function() {
+
     $('textarea').focus();
+
   });
 
   // Hide the error display on load
@@ -18,28 +19,34 @@ $(document).ready(function() {
 
   // Filtering text for XSS prevention
   const escape =  function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
 
-  // Loops through an array of tweets 
+    let div = document.createElement('div');
+
+    div.appendChild(document.createTextNode(str));
+
+    return div.innerHTML;
+  };
+
+  // Loops through an array of tweets
   // to createTweetElement
   const renderTweets = (tweetArray) => {
+
     $('#tweets-container').empty()
 
     for (let eachTweet of tweetArray) {
-      console.log(`each tweet`, eachTweet)
+
       $tweetObj = createTweetElement(eachTweet);
+
     }
-  }
+  };
 
   // Creates dynamic tweets via HTML Markup
   // to the #tweets-container id in index.html
   const createTweetElement = (tweetObj) => {
+
     const avatar = "far fa-meh fa-2x";
     const tweetTime = moment(tweetObj.created_at).fromNow(true);
-    console.log(`tweet time`, tweetTime)
+
     let $tweet = $(` 
     <article class="border">
       <div>
@@ -66,17 +73,17 @@ $(document).ready(function() {
       </div>
     </article>`);
     $('#tweets-container').prepend($tweet);
-  }
+  };
 
   // Use ajax to send form && prevent
   // page reload
   $("form").submit(function(event) {
+
     event.preventDefault();
 
     const targetValue = $(this).find("textarea").val();
-    console.log(`target value`, targetValue)
-    // if blah blah blah slide down error
     
+    // Error prompts
     if (targetValue.length > 140) {
       $('#error').slideDown();
       $('#error').html("🤐 There <b>is</b> such a thing as <i>over</i>sharing. Dial it back a bit.");
@@ -87,19 +94,19 @@ $(document).ready(function() {
 
     } else {
       $.ajax({ type: "POST", url: "/tweets", data: $(this).serialize() })
-      .then(console.log("AJAX post tweet"))
-      .then(loadTweets())
+        .then(console.log("AJAX post tweet"))
+        .then(loadTweets());
     }
   });
 
   // Use ajax to load the database
   const loadTweets = () => {
     $.ajax('/tweets', { method: 'GET'})
-    .then((fetchTweets) => {
-      console.log(`Got the tweets`, fetchTweets);
-      renderTweets(fetchTweets)
-    });
-  }
+      .then((fetchTweets) => {
+        console.log(`Got the tweets`, fetchTweets);
+        renderTweets(fetchTweets);
+      });
+  };
 
   loadTweets();
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,12 @@
-console.log(`composer-char-counter loaded`)
 
 $(document).ready(function() {
-  console.log(`Ready Composer One`)
-  // Select the id to assign the event listener 
+
+  // Select the id to assign the event listener
   // Use "input" so it update on every char change
-  $(".tweet-text").on("input", function () {
-    // change the height of the text box (stretch)
-    // $('.tweet-text')[0].scrollHeight;
+  $(".tweet-text").on("input", function() {
 
     // Length is the value of #text-tweet chars
     let length = $(this).val().length;
-    console.log($(this).val())
 
     // Grab the value of counter
     let counter = $(".counter");
@@ -18,23 +14,15 @@ $(document).ready(function() {
 
     // Change the counter to red if
     // they are over the character limit
-    console.log(length)
     if (length > 140) {
       counter.addClass("red");
 
     } else if (length < 141) {
       counter.removeClass("red");
       $('#error').slideUp(1000);
+
     } else if (length > 0) {
       $('#error').slideUp(1000);
     }
-  })
-
-  $(".tweet-text").on("submit", () => {
-    if (length > 140) {
-      return alert("Too many characters")
-    } else if (length < 1 || length === null || length === "") {
-      return alert("Please, write... something")
-    }
-  })
+  });
 });
